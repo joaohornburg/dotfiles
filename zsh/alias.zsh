f58b900d6e475885d4f7af83d1ef9b62b15f3c06
joaohornburg <joao.hornburg@gmail.com>
2013-08-30 16:17:29 -0300

    add alias to reboot services and to start RD Station

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,6 +1,8 @@
 alias reload='source ~/.zshrc'
 alias dotconfig="cd $DOTFILES"
 alias srv='./script/server'
+alias station='ws rdstation && srv'
+alias reboot_services='boxen --disable-services && boxen --enable-services '
 
 # List direcory contents
 alias l='ls -lA'
